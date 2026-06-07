7f1e49a24639c8ce556ff584f788bae641f83422
Yash-kumar0 <25113cn387@glbitm.ac.in>
2026-06-07 16:42:28 +0530

    Add files via upload

---
+++ Day-9/Q35.cpp
@@ -0,0 +1,31 @@
+/*Write a program to Print repeated character
+pattern.
+A
+BB
+CCC
+DDDD
+EEEEE*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter the number :: ";
+    cin >> n;
+
+    char ch='A';
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+            cout << ch;
+        }
+        ch++;
+        cout << endl;
+    }
+    return 0;
+}
